8e06f14068d266effd77c82149a76b581763008a
Andrew Feeney <deskspace@gmail.com>
2015-08-03 11:44:23 +1000

    Added some shortcakes.

---
+++ includes/dev-helpers.php
@@ -0,0 +1,22 @@
+<?php
+/* 
+ * dev-helpers.php
+ *
+ * WebSpanner Development Helper Functions 
+ *
+ */
+ 
+// Lists all available shortcodes shortcodes
+function list_shortcodes()
+{
+	global $shortcode_tags;
+	echo "<pre>"; print_r($shortcode_tags); echo "</pre>";
+}
+add_shortcode('list_shortcodes', 'list_shortcodes');
+
+
+
+
+
+
+?>
