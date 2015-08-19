a450b26a84f595015a70f9db7594e6bb19fd1975
WebSpanner <webspannerdev@gmail.com>
2015-08-19 12:21:59 +1000

    Changed version header to 0.2.2

--- ws-schema-tools.php
+++ ws-schema-tools.php
@@ -4,7 +4,7 @@ Plugin Name: WebSpanner - SchemaTools
 Plugin URI: http://www.makeweb.com.au
 Description: Add schema.org microdata to your wordpress site using shortcodes.
 Author: Andrew Feeney
-Version: 0.2.1
+Version: 0.2.2
 Author URI: http://www.makeweb.com.au
 GitHub Plugin URI: https://github.com/WebSpanner/SchemaTools
 GitHub Branch: master
